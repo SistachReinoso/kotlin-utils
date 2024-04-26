14c709bdf574ed697bf487b0173eb3fddac1c3ee
Arnau Sistach <Arnau.Sistach@entrust.com>
2024-04-26 14:16:07 +0200

    latest version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 plugins {
+    alias(libs.plugins.versions)
     kotlin("jvm") version "1.9.23" apply false
 }
 
@@ -13,4 +16,18 @@ subprojects {
     tasks.withType<Test> {
         useJUnitPlatform()
     }
+}
+
+// https://github.com/ben-manes/gradle-versions-plugin
+tasks.withType<DependencyUpdatesTask> {
+    rejectVersionIf {
+        isNonStable(candidate.version) && !isNonStable(currentVersion)
+    }
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
 }
