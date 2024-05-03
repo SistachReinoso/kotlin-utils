ac391b5c7cae80d85e6596c9e3f3b56878fad63b
Sistach Reinoso <sistacharnau@gmail.com>
2024-05-03 11:21:30 +0200

    ups

--- command/src/main/kotlin/org/linux/command/update/UpdateCommand.kt
+++ command/src/main/kotlin/org/linux/command/update/UpdateCommand.kt
@@ -8,5 +8,6 @@ class UpdateCommand: CliktCommand() {
     private val password by option().prompt(hideInput = true)
     override fun run() {
         echo("your password is $password")
+        echo("ups, that normally is a secret")
     }
 }
