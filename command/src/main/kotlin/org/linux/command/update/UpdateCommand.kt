06bf49a973561ea571d4e788e3495a0d574b3ccc
Sistach Reinoso <sistacharnau@gmail.com>
2024-05-08 16:14:41 +0200

    pendnet logs
    queda poc aparentment pel update ben fet

--- command/src/main/kotlin/org/linux/command/update/UpdateCommand.kt
+++ command/src/main/kotlin/org/linux/command/update/UpdateCommand.kt
@@ -1,13 +1,8 @@
 package org.linux.command.update
 
 import com.github.ajalt.clikt.core.CliktCommand
-import com.github.ajalt.clikt.parameters.options.option
-import com.github.ajalt.clikt.parameters.options.prompt
-import com.github.ajalt.clikt.parameters.options.required
 
 class UpdateCommand : CliktCommand() {
-    private val password by option().prompt(hideInput = true)
-    private val name by option(envvar = "USERNAME").required()
     override fun run() {
         update()
     }
--- lib/src/main/kotlin/org/linux/command/update/Update.kt
+++ lib/src/main/kotlin/org/linux/command/update/Update.kt
@@ -1,10 +1,17 @@
 package org.linux.command.update
 
-enum class UpdateState(val state: String) {
-    UP_TO_DATE("All packages are up to date."),
+import kotlinx.coroutines.*
+
+enum class UpdateState(val state: Regex) {
+    UP_TO_DATE("All packages are up to date.".toRegex()),
+    CAN_BE_UPGRADED("""\d+ package can be upgraded. Run 'apt list --upgradable' to see it.""".toRegex())
 }
 
-fun update() {
+fun update() = runBlocking {
+    extracted()
+}
+
+private suspend fun extracted() = coroutineScope {
     val command = listOf("sudo", "apt", "update")
 
     val proc = ProcessBuilder(command)
@@ -12,15 +19,30 @@ fun update() {
         .redirectError(ProcessBuilder.Redirect.PIPE)
         .start()
 
-    launch {
-
-    }
     var count = 0
-    val a = proc.inputStream.bufferedReader().lineSequence()
-    val b = proc.errorStream.bufferedReader().lineSequence()
-    for (line in b)
-        println("E: ${++count}\tline: $line")
-    for (line in a) {
-        println("M: ${++count}\tline: $line")
+    launch {
+        proc.errorStream
+            .bufferedReader()
+            .lineSequence()
+            .filter { it.isNotBlank() }
+            .forEach { line -> println("E: ${++count}\tline: $line") }
     }
+
+    val result = async {
+        proc.inputStream
+            .bufferedReader()
+            .lineSequence()
+            .map { line -> println("M: ${++count}\tline: $line"); line.trim() }
+            .filter { it.isNotBlank() }
+            .last()
+    }
+
+    val result2 = result.await()
+    println("result: $result2")
+    val a = UpdateState.entries
+        .first { it.state.matches(result2) }
+    println("update State=$a")
+
+    proc.waitFor()
+    println("Exit status: ${proc.exitValue()}")
 }
